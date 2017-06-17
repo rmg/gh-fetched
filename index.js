1478a241e01992e2507a864b97cc7a3a48298d33
Ryan Graham <r.m.graham@gmail.com>
2017-06-17 11:55:52 -0700

    add simple token auth support

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 'use strict';
 
+const debug = require('debug')('gh-fetched');
 const fetch = require('make-fetch-happen');
+const lo = require('lodash');
+
+const DEFAULTS = {
+  root: 'https://api.github.com',
+};
 
 class GitHub {
-  constructor() {
-    this.root = 'https://api.github.com';
+  constructor(opts) {
+    opts = lo.defaults(DEFAULTS, opts);
+    this.root = opts.root;
+    this.token = opts.token;
+    let headers = {};
+    if (this.token) {
+      headers['Authorization'] = `token ${this.token}`;
+    }
+    this.fetch = fetch.defaults({headers: headers});
+    this.quota = {};
   }
 
   get(reqPath) {
     let reqUrl = this.root + reqPath;
-    return fetch(reqUrl).then(res => res.json());
+    return this.fetch(reqUrl).then(res => {
+      this.quota['limit'] = 0|res.headers.get('x-ratelimit-limit');
+      this.quota['remaining'] = 0|res.headers.get('x-ratelimit-limit');
+      this.quota['reset'] = new Date((0|res.headers.get('x-ratelimit-reset')) * 1000);
+      debug('ratelimit:', this.quota);
+      return res.json();
+    });
   }
 }
 
---
+++ test/test-auth.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const test = require('tap').test;
+const GitHub = require('../');
+
+const maybeSkip = {};
+if (!process.env.GITHUB_TOKEN) {
+  maybeSkip = 'env does not hvae GITHUB_TOKEN set, skipping';
+}
+
+test('basic API', maybeSkip, t => {
+  const authed = new GitHub({token: process.env.GITHUB_TOKEN});
+  return authed.get('/').then(r => {
+    t.ok(r.current_user_url, 'resonse looks like the GH API root');
+    t.equal(authed.quota.limit, 5000, 'should have higher quota');
+  });
+});
